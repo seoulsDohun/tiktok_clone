ac0cea6200b5002c9e889bf9a707adf61cf8403b
dohuns <gkgk586@gmail.com>
2024-01-09 00:00:28 +0900

    틱톡 클론코딩 > signUp화면 시작

---
+++ lib/screens/sign_up/sign_up_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: Sizes.size40,
+          ),
+          child: Column(children: [
+            Gaps.v80,
+            Text(
+              'Sign Up for TikTok',
+              style: TextStyle(
+                fontSize: Sizes.size24,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Gaps.v16,
+            Text(
+              'Create a profile, follow other accounts, make your own videos, and more.',
+              style: TextStyle(
+                fontSize: Sizes.size16,
+                color: Colors.black45,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ]),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        elevation: 2,
+        color: Colors.grey.shade100,
+        child: Padding(
+          padding: const EdgeInsets.only(
+            top: Sizes.size20,
+            bottom: Sizes.size40,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Already have an account?',
+                style: TextStyle(
+                  fontSize: Sizes.size16,
+                ),
+              ),
+              Gaps.h5,
+              Text(
+                'Log in',
+                style: TextStyle(
+                  fontSize: Sizes.size16,
+                  fontWeight: FontWeight.w600,
+                  color: Theme.of(context).primaryColor,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
